201c26f51c42551af68d0fccb8964a4c751a8ae3
Piyush Singh <1806397@kiit.ac.in>
2023-07-26 16:46:07 +0530

    Added CSS modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,13 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
 function Homepage() {
-  return <div>Worldwise</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Worldwise</h1>
+      <Link to="/app">Go to the app</Link>
+    </div>
+  );
 }
 export default Homepage;
