6e6ec1a7074bb1d0d8f889bf312d94c84fcf4efc
Piyush Singh <1806397@kiit.ac.in>
2023-07-28 12:41:28 +0530

    Created Sidebar

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,18 @@
+import styles from "./SideBar.module.css";
+import AppNav from "./AppNav.jsx";
+import Logo from "./Logo";
+function SideBar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
+}
+export default SideBar;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,14 @@
-import AppNav from "../components/AppNav";
-
+// import AppNav from "../components/AppNav";
+import SideBar from "../components/SideBar";
+import styles from "./AppLayout.module.css";
 function AppLayout() {
   return (
     <div>
-      <AppNav />
-      <p>App</p>
+      {/* <AppNav />
+      <p>App</p> */}
+      <div className={styles.app}>
+        <SideBar />
+      </div>
     </div>
   );
 }
