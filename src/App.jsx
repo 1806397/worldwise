b071760892c828cadff3fe844868d077a0b8874b
Piyush Singh <1806397@kiit.ac.in>
2023-07-28 14:56:25 +0530

    Implemented country list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Product from "../src/pages/Product";
 import Pricing from "../src/pages/Pricing";
 import Homepage from "../src/pages/Homepage";
@@ -6,8 +7,10 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountriesList from "./components/CountryList";
+
 const BASE_URL = "http://localhost:8000";
+
 function App() {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -42,7 +45,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           /> */}
-          <Route path="countries" element={<p>Country</p>} />
+          <Route
+            path="countries"
+            element={<CountriesList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>form</p>} />
         </Route>
         <Route path="login" element={<Login />} />
