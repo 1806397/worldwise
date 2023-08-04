1ea8b4966cf26ec9d5fa0c99d3b65334841e88c2
Piyush Singh <1806397@kiit.ac.in>
2023-08-04 22:15:50 +0530

    Minor change

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -33,8 +33,8 @@ function Map() {
         className={styles.map}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://carto.com/">carto.com</a> contributors'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png"
         />
         {cities.map((city) => (
           <Marker
